72e480a4667976e57fc34c1aa935d96175e918ca
george <jorgelitox15@gmail.com>
2023-11-16 23:58:57 -0500

    Added devise-jwt for generate token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# RegistrationsController handles user registrations.
+
 class UsersController < ApplicationController
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  def token
+    JWT.encode({ user_id: id }, Rails.application.secrets.secret_key_base)
+  end
+
 end
